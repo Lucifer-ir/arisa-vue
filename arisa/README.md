d23cdaff59b55fde5aa6d701852eef22985cbf9d
beyramijamdev-cell <beyramijam.dev@gmail.com>
2025-11-19 20:48:03 +0330

    نصب vue خام

--- arisa/README.md
+++ arisa/README.md
@@ -1,4 +1,4 @@
-# arisa_vue
+# arisa
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- arisa/src/main.js
+++ arisa/src/main.js
@@ -1,6 +1,4 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@flaticon/flaticon-uicons/css/all/all.css';
+
 createApp(App).mount('#app')
