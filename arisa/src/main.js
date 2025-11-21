d2f333e4c4ec1fa60f6f77114d7e064763c239f2
beyramijamdev-cell <beyramijam.dev@gmail.com>
2025-11-21 20:01:47 +0330

    اضافه کردن سایدبار

--- arisa/src/main.js
+++ arisa/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Saidbar from './pages/Saidbar.vue'
+import Dashboard from './pages/Dashboard.vue'
+import './styles/main.css'
 
 createApp(App).mount('#app')
+createApp(Saidbar).mount('#saidbar')
+createApp(Dashboard).mount('#dashboard')
---
+++ arisa/src/pages/Saidbar.vue
@@ -0,0 +1,70 @@
+<script setup></script>
+
+<template>
+    <div class="w-70 bg-gray-300 rounded-lg p-3 grid gride-col-1 items-center justify-center m-3">
+        <div class="user w-100 flex justify-center mb-3">
+            <i class="fi fi-rs-user p-3 bg-black text-white rounded-lg w-12 h-12 flex items-center justify-center text-center"></i>
+        </div>
+        <span class="text-center">محمد بیرامی جم</span>
+        <div class="flex justify-center mt-3">
+            <a href="#" class=" bg-gray-500 rounde-md px-2 py-1 rounded-sm text-white">شارژ کیف پول</a>
+        </div>
+    </div>
+    <div class="w-70 bg-gray-500 rounded-lg m-3 p-3">
+        <div class="bg-gray-300 rounded-md p-3 mb-5">
+            <div class="flex items-center gap-2">
+                <i class="fi fi-rs-user p-3 bg-black text-white rounded-lg w-12 h-12 flex items-center justify-center"></i>
+                <div class="grid-cols-1 grid">
+                    <span>200,000,000</span>
+                    <span>تومان</span>
+                </div>
+            </div>
+        </div>
+        <nav>
+            <ol>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href="">داشبورد</a>
+                    <i class="fi fi-rs-user"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> سرویس ها</a>
+                    <i class="fi fi-rs-apps"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> دامنه ها</a>
+                    <i class="fi fi-rs-apps"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> صورت حساب ها</a>
+                    <i class="fi fi-rs-apps"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> تیکت ها</a>
+                    <i class="fi fi-rs-apps"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> کیف پول</a>
+                    <i class="fi fi-rs-apps"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> حساب کاربری</a>
+                    <i class="fi fi-rs-edit"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> پیام ها</a>
+                    <i class="fi fi-rs-apps"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between mb-2">
+                    <a href=""> همکاری در فروش</a>
+                    <i class="fi fi-rs-apps"></i>
+                </li>
+                <li class="bg-gray-300 rounded-md p-2 flex align-center justify-between">
+                    <a href=""> خروج</a>
+                    <i class="fi fi-rs-log-out"></i>
+                </li>
+            </ol>
+        </nav>
+    </div>
+</template>
+
+<style scoped></style>
